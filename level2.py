8b5e176c5cdb433d41de70e7058f7bbab448a066
Filip Jaskovic <75838849+FilipJaskovic@users.noreply.github.com>
2023-10-22 23:18:27 +0200

    Update level2.py

--- level2.py
+++ level2.py
@@ -1,21 +1,13 @@
-import os
 from collections import Counter
 
 def normalize_piece(piece):
-
     piece_chars = piece.split(',')
     rotations = [piece_chars[i:] + piece_chars[:i] for i in range(4)]
-
     rotation_strs = [','.join(rotation) for rotation in rotations]
     rotation_strs.sort()
-    
     return rotation_strs[0]
 
-def count_puzzle_pieces(file_path, output_file_path):
-    if not os.path.isfile(file_path):
-        print(f"File not found: {file_path}")
-        return
-
+def count_puzzle_pieces(file_path):
     with open(file_path, 'r') as file:
         lines = file.readlines()
 
@@ -27,11 +19,14 @@ def count_puzzle_pieces(file_path, output_file_path):
 
     output_lines = [f"{count} {piece}" for piece, count in piece_counts.items()]
 
+    output_file_path = file_path.replace('.in', '-output.txt')
     with open(output_file_path, 'w') as output_file:
         output_file.write('\n'.join(output_lines))
 
-    print(f"Output written to {output_file_path}")
+def main():
+    for i in range(5):
+        file_path = f'level2_{i+1}.in'
+        count_puzzle_pieces(file_path)
 
-input_file_path = 'level2_5.in'
-output_file_path = 'level2_5-output.txt' 
-count_puzzle_pieces(input_file_path, output_file_path)
+if __name__ == "__main__":
+    main()
